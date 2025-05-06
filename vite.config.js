70867da17ea0108f93fee1a7e218a44ef06aa7ab
Abel Mawuse Amedor <111967490+Maxbwoy@users.noreply.github.com>
2025-05-06 23:52:38 +0000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss({
+      config: {
+        content: [
+          './index.html',
+          './src/**/*.{js,ts,jsx,tsx}',
+        ],
+        theme: {
+          extend: {
+            colors: {
+              'blue-600': '#3B82F6',
+              'purple-600': '#9333EA',
+              'white': '#FFFFFF',
+              'gray-50': '#F9FAFB',
+              'gray-600': '#4B5563',
+              'green-600': '#10B981',
+              'green-700': '#059669',
+            },
+          },
+        },
+      },
+    })
+  ],
 })
